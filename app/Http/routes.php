6a6c1bd419ad47fa84ed320938e83ed7a1aaf20e
Zeuxis Lo <seekstudio@gmail.com>
2016-03-22 15:20:06 +0800

    add jwt auth protect in api.panel.news.all entry point

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,7 +39,7 @@ $api->version('v1', function($api) {
         $api->post('signin', ['as' => 'api.home.signin', 'uses' => 'HomeController@signin']);
     });
 
-    $api->group(['namespace' => 'App\Api\Version1\Controllers\Panel', 'prefix' => 'panel'], function($api) {
+    $api->group(['namespace' => 'App\Api\Version1\Controllers\Panel', 'prefix' => 'panel', 'middleware' => 'api.auth'], function($api) {
         $api->group(['prefix' => 'news'], function($api) {
             $api->get('all', ['as' => 'api.panel.news.all', 'uses' => 'NewsController@all']);
         });
