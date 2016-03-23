36f57e142ac287c58b4765dfc88b6288551a8300
Zeuxis Lo <seekstudio@gmail.com>
2016-03-23 18:00:27 +0800

    change bootstrap can

--- resources/views/layout/backend.blade.php
+++ resources/views/layout/backend.blade.php
@@ -4,10 +4,10 @@
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>Kauppaa</title>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{ asset('assets/css/backend.css') }}" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="navbar navbar-static-top navbar-default navbar-inverse" role="navigation">
