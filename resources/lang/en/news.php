b802e5dff104a5c703d95ec7646a7e75b242e56c
Zeuxis Lo <seekstudio@gmail.com>
2016-03-26 12:35:23 +0800

    let news page translatable

---
+++ resources/lang/en/news.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'show' => [
+        'title' => 'News'
+    ]
+];
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-md-9">
                     <div class="panel panel-default">
-                        <div class="panel-heading">News</div>
+                        <div class="panel-heading">{{ trans('news.show.title') }}</div>
                         <div class="panel-body">
                             <h3 class="title">{{ $news->title }}</h3>
                             <div class="status">
